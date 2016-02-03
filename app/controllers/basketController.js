57597a7b0592c6196b9133890dcb9ef2dd3b50f0
danwheeler <da-wheeler@hotmail.co.uk>
2016-02-03 21:25:17 +0000

    added flash messages and timeout function

--- app/controllers/basketController.js
+++ app/controllers/basketController.js
@@ -1,27 +1,37 @@
 'use strict';
 
-fashionRetailer.controller('BasketController', ['$http', 'ResourceFactory', 'ShoppingBasketService', function($http, ResourceFactory, ShoppingBasketService) {
+fashionRetailer.controller('BasketController', ['$http', 'ResourceFactory', 'ShoppingBasketService', '$timeout', function($http, ResourceFactory, ShoppingBasketService, $timeout) {
 	var self = this;
 
 	self.shoppingBasketService = ShoppingBasketService;
 	self.resourceFactory = ResourceFactory;
 	self.basket = self.shoppingBasketService.basket;
 	self.basketInfo = self.shoppingBasketService.basketInfo;
+	self.flashMessage = 0;
 
 	self.resourceFactory.query("discountCodes")
 		.then(function(response) {
 			self.discountCodes = response.data;
 		});
 
+	$timeout( function(){ self.resetFlashMessage(); }, 10000);
+
 	self.addItem = function(item) {
-		if (!("quantity" in item) || (item.quantity === 0)) {
-			self.shoppingBasketService.addNewItem(item);
+		if (item.stockQuantity > 0) {
+			if (!("quantity" in item) || (item.quantity === 0)) {
+				self.shoppingBasketService.addNewItem(item);
+			} 
+		} else {
+			self.flashMessage = 1;
 		}
+
 	};
 
 	self.addOne = function(item) {
 		if ((item.quantity + 1) <= item.stockQuantity) {
 			self.shoppingBasketService.addOne(item);
+		} else {
+			self.flashMessage = 1;
 		}
 	};
 
@@ -37,6 +47,8 @@ fashionRetailer.controller('BasketController', ['$http', 'ResourceFactory', 'Sho
 		for (var i = 0; i < self.discountCodes.length; i++) {
 			if (usercode === self.discountCodes[i].code) {
 				self.shoppingBasketService.addDiscount(usercode, self.discountCodes[i]);
+			} else {
+				self.flashMessage = 2;
 			}
 		}
 	};
@@ -44,6 +56,14 @@ fashionRetailer.controller('BasketController', ['$http', 'ResourceFactory', 'Sho
 	self.update = function() {
 		self.shoppingBasketService.addPrice();
 		self.shoppingBasketService.calcTotalPrice(); 
+	};
+
+	self.resetFlashMessage = function() {
+		self.flashMessage = 0;
+	};
+
+	self.activeFlashMessage = function(value) {
+		return (self.flashMessage === value);
 	};
 
 }]);
