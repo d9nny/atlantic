b8af41bec6f89c5f00c05cd372c33204ab951c47
danwheeler <da-wheeler@hotmail.co.uk>
2016-02-09 17:54:49 +0000

    changed ng-include to directives

---
+++ app/directives/directives.js
@@ -0,0 +1,29 @@
+fashionRetailer.directive('basket', function() {
+	return {
+		templateUrl: 'app/partials/basket.html'
+	};
+});
+
+fashionRetailer.directive('flashMessages', function() {
+	return {
+		templateUrl: 'app/partials/flashMessages.html'
+	};
+});
+
+fashionRetailer.directive('navbar', function() {
+	return {
+		templateUrl: 'app/partials/navbar.html'
+	};
+});
+
+fashionRetailer.directive('products', function() {
+	return {
+		templateUrl: 'app/partials/products.html'
+	};
+});
+
+fashionRetailer.directive('topSection', function() {
+	return {
+		templateUrl: 'app/partials/topSection.html'
+	};
+});
