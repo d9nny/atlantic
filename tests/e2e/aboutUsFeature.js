7cf09c3d549c6463d419372942253178a4dc93a7
danwheeler <da-wheeler@hotmail.co.uk>
2016-02-23 17:49:25 +0000

    heroku push working

--- tests/e2e/aboutUsFeature.js
+++ tests/e2e/aboutUsFeature.js
@@ -9,17 +9,4 @@ describe('About Us', function() {
   it('has a title', function() {
     expect(browser.getTitle()).toEqual('atlantic');
   });
-  describe('opening basket', function(){
-  it('clicking on basket icon opens the basket', function() {
-    basket.click();
-    expect(element(by.id('basket'))).isDisplayed();
-  });
-
-  it('finds the last Spike', function() { 
-    searchBox.sendKeys('spike');
-    searchButton.click();
-
-    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-    expect(profiles.last().getText()).toEqual('spikeh'); 
-
 });
